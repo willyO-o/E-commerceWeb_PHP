a1cb2df6f14b6366329fe3aa983ad3473161596b
willyO-o <marcggmarc.20@gmail.com>
2019-11-24 19:00:04 -0400

    commit

--- inc/layout/footer.php
+++ inc/layout/footer.php
@@ -306,12 +306,12 @@
 					<div class="row">
 						<div class="col-md-12 text-center">
 							<ul class="footer-payments">
-								<li><a href="#"><i class="fa fa-cc-visa"></i></a></li>
-								<li><a href="#"><i class="fa fa-credit-card"></i></a></li>
-								<li><a href="#"><i class="fa fa-cc-paypal"></i></a></li>
-								<li><a href="#"><i class="fa fa-cc-mastercard"></i></a></li>
-								<li><a href="#"><i class="fa fa-cc-discover"></i></a></li>
-								<li><a href="#"><i class="fa fa-cc-amex"></i></a></li>
+								<li><a ><i class="fa fa-cc-visa"></i></a></li>
+								<li><a ><i class="fa fa-credit-card"></i></a></li>
+								<li><a ><i class="fa fa-cc-paypal"></i></a></li>
+								<li><a ><i class="fa fa-cc-mastercard"></i></a></li>
+								<li><a ><i class="fa fa-cc-discover"></i></a></li>
+								<li><a ><i class="fa fa-cc-amex"></i></a></li>
 							</ul>
 							<span class="copyright">
 								<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
